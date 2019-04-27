1875f9a71b2464980e6f804ae9afba06997ea288
Danylo Stasenko <danylo.stasenko@gmail.com>
2019-04-28 00:21:31 +0300

    Deleted all unused files, cleaned up code

--- tensorflow/script.py
+++ tensorflow/script.py
@@ -1,30 +1,28 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.python.platform import gfile
-GRAPH_PB_PATH = 'frozen_graph_def.pb'
+
+GRAPH_PB_PATH = 'model/cube.pb'
+
+in_tensor_observations = 'vector_observation:0'
+in_tensor_actions = 'action_masks:0'
+output_tensor_actions = 'action:0'
+
+def getAction(x, y):
+    result = 0
+    if (x - y > 1):
+        result = 1
+    return result
+
 with tf.Session() as sess:
-   print("load graph")
-   with gfile.FastGFile(GRAPH_PB_PATH,'rb') as f:
+   with gfile.FastGFile(GRAPH_PB_PATH,'rb') as file:
        graph_def = tf.GraphDef()
-   graph_def.ParseFromString(f.read())
+   graph_def.ParseFromString(file.read())
    sess.graph.as_default()
    tf.import_graph_def(graph_def, name='')
-   graph_nodes=[n for n in graph_def.node]
-   names = []
-   for t in graph_nodes:
-      names.append(t.name)
-   print(names)
 
-# These names are part of the model and cannot be changed.
-output_layer = 'action:0'
-input_node = 'vector_observation:0'
-input_node2 = 'action_masks:0'
-
-
-with tf.Session() as sess:
-   import numpy as geek
-
-   observations = geek.empty(6, dtype=float)
-   masks = geek.empty(8, dtype=float)
+   observations = np.empty(2, dtype=float)
+   masks = np.empty(8, dtype=float)
    masks[0] = 1
    masks[1] = 1
    masks[2] = 1
@@ -34,40 +32,18 @@ with tf.Session() as sess:
    masks[6] = 1
    masks[7] = 1
 
-   observations[0] = -0.9
-   observations[1] = -0.1
-   observations[2] = -0.1
-   observations[3] = -0.3
-   observations[4] = -0.1
-   observations[5] = -0.1
+   observations[0] = 0.8
+   observations[1] = -0.5
 
    try:
-      prob_tensor = sess.graph.get_tensor_by_name(output_layer)
-      predictions, = sess.run(prob_tensor, {input_node: [observations], input_node2:[masks]})
-      print("Results:")
-      print(predictions[0])
-      print(predictions[1])
-      print(predictions[2])
-      print(predictions[3])
-      print(predictions[4])
-      print(predictions[5])
-      print(predictions[6])
-      print(predictions[7])
+       prob_tensor = sess.graph.get_tensor_by_name(output_tensor_actions)
+       result, = sess.run(prob_tensor, {in_tensor_observations: [observations], in_tensor_actions: [masks]})
+       print("In:")
+       print("", observations[0], observations[1])
+       print("Out:")
+       print("", getAction(result[0], result[1]), getAction(result[2], result[3]), getAction(result[4], result[5]),
+             getAction(result[5], result[7]))
    except KeyError:
-      print("Couldn't find classification output layer: " + output_layer + ".")
-      print("Verify this a model exported from an Object Detection project.")
-      exit(-1)
-
-# todo: Continue playing with input
-#  Got first results:
-# -22.331755
-# 0.0
-# 0.0
-# -22.332441
-# -0.89462394
-# -0.52553546
-# -2.0909839
-# -0.1318933
-
-# need to investigate how they were formed
-# and how it will change according to changed input values
+       print("Couldn't find classification output layer: " + output_tensor_actions + ".")
+       print("Verify this a model exported from an Object Detection project.")
+       exit(-1)
